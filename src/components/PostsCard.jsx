7f09307fe271eeb358cef4aa635902e7e12fc6ad
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-19 18:43:11 +0200

    [UPD] stilizzazioni in css

--- src/components/PostsCard.jsx
+++ src/components/PostsCard.jsx
@@ -6,11 +6,11 @@ const PostsCard = (props) => {
 
     return (
         // definisco la struttura delle card
-        <div className="col-6 mb-4">
+        <div className="col-lg-6 col-md-10 col-sm-12 mb-4">
             <div className="card h-100">
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
-                    <h6 className="card-subtitle text-body-secondary">{category}</h6>
+                    <h6 className="card-subtitle text-body-secondary mt-3">{category}</h6>
                     <p className="card-text">{content}.</p>
                 </div>
             </div>
